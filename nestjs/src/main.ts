9f9582ad17e1f01e7e11b7507fc202f049b7cc18
saarikoski-jules <jules.saarikoski@gmail.com>
2021-07-28 12:46:59 +0200

    Feat: Added configuration for database connection, utils and dependencies

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { ConnectionOptions } from 'tls';
 import { AppModule } from './app.module';
+import { getDbConnectionOptions, runDbMigrations } from './shared/utils';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule.forRoot(await getDbConnectionOptions()));
+	await runDbMigrations();
+	await app.listen(3000);
 }
+
 bootstrap();
