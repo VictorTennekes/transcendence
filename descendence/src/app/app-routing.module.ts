c4fd4f1933d8efbe42700ba0b72b159cbc2db14f
saarikoski-jules <jules.saarikoski@gmail.com>
2021-07-31 16:54:56 +0200

    feat: added test chat component to frontend

--- descendence/src/app/app-routing.module.ts
+++ descendence/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MasterComponent } from './master/master.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { MatInputModule } from '@angular/material/input'
 
 const routes: Routes = [
 	{
@@ -16,11 +18,17 @@ const routes: Routes = [
 	{
 		path: 'login',
 		component: LoginComponent
+	},
+    {
+		path: 'chat',
+		component: ChatComponent
 	}
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
+	imports:
+        [RouterModule.forRoot(routes),
+        MatInputModule],
 	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- descendence/src/app/app.module.ts
+++ descendence/src/app/app.module.ts
@@ -8,19 +8,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { MasterComponent } from './master/master.component';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ChatComponent } from './components/chat/chat.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HomeComponent,
 		MasterComponent,
-  LoginComponent
+  LoginComponent,
+  ChatComponent
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		HttpClientModule,
-		ReactiveFormsModule
+		ReactiveFormsModule,
+		BrowserAnimationsModule,
+        MatInputModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
