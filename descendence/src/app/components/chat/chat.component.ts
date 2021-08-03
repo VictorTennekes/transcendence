e29ee4d0929ee86c4f4dc09eb0c88305e92f8c5a
saarikoski-jules <jules.saarikoski@gmail.com>
2021-08-03 12:15:23 +0200

    feat: getting data to frontend app from form

--- descendence/src/app/components/chat/chat.component.ts
+++ descendence/src/app/components/chat/chat.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { DoAction } from './action';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        private formBuilder: FormBuilder
+    ) { }
+    messageForm = this.formBuilder.group({
+        message: "",
+    });
+    @Output() public action: EventEmitter<DoAction> = new EventEmitter();
 
   ngOnInit(): void {
   }
+  public onSubmit() {
+      console.log("submitting");
+      console.log(this.messageForm);
+      this.action.emit({
+          type: 'send-message',
+          payload: this.messageForm
+      });
+      this.messageForm.reset();
+    }
 
 }
