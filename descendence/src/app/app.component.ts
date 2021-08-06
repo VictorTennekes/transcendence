70575b424608f03562accb4f3c42f853472a38d5
saarikoski-jules <jules.saarikoski@gmail.com>
2021-08-06 19:23:05 +0200

    feat: testing layout

--- descendence/src/app/app.component.ts
+++ descendence/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+export interface Tile {
+    color: string;
+    cols: number;
+    rows: number;
+    text: string;
+  }
+  
+  /**
+   * @title Dynamic grid-list
+   */
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component1.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Descendence';
+    tiles: Tile[] = [
+      {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+      {text: 'Two', cols: 2, rows: 1, color: 'lightgreen'},
+      {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    ];
 }
