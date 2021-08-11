157ca2368661e9bf641ae305c5b03f2eac1c73a7
saarikoski-jules <jules.saarikoski@gmail.com>
2021-08-11 16:14:48 +0200

    chore: removed unused module

--- descendence/src/app/app.module.ts
+++ descendence/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
@@ -14,7 +13,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
     BrowserModule,
     AppRoutingModule,
     MatGridListModule,
-    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
