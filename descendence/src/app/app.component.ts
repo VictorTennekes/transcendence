a3e560838ca0b7f0b68b7db9a786fafd5d1e8eb9
saarikoski-jules <jules.saarikoski@gmail.com>
2021-08-11 15:53:58 +0200

    feat: added partitions for different components

--- descendence/src/app/app.component.ts
+++ descendence/src/app/app.component.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
 
-export interface Tile {
-    color: string;
-    cols: number;
-    rows: number;
-    text: string;
-  }
-  
-  /**
-   * @title Dynamic grid-list
-   */
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component1.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Descendence';
-    tiles: Tile[] = [
-      {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
-      {text: 'Two', cols: 2, rows: 1, color: 'lightgreen'},
-      {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    ];
 }
