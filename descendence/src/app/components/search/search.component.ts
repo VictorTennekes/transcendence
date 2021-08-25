57aaf4d19876e4f7a9160740ec23b37b14f065a2
saarikoski-jules <jules.saarikoski@gmail.com>
2021-08-25 09:26:46 +0200

    feat: error appears when user isn't found

--- descendence/src/app/components/search/search.component.ts
+++ descendence/src/app/components/search/search.component.ts
@@ -26,112 +26,16 @@ import { nullSafeIsEquivalent } from "@angular/compiler/src/output/output_ast";
 
     public chatId: string = "";
 
-	// userForm = new FormGroup ({
-	// 	username: new FormControl('', [
-	// 		Validators.required,
-	// 		this.userValidator
-	// 	]),
-	// }, {updateOn: "submit"})
-
-
-
-	validateAsync(control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
-	// validateAsync(control: AbstractControl): Observable<ValidationErrors> {
-
-		// let http: HttpClient
-
-		// let val: boolean = false;
-		// this.http.get('api/chat/find/' + control.value).subscribe(
-			// (response) => null,
-			// (error) => of({'invalid': true}),
-		// );
-		// console.log('here');
-		// if (control.touched) {
-			console.log('after');
-			console.log(control.value);
-			// return this.http.get('api/chat/find/' + control.value).pipe(
-			// (response) => {
-			// 	console.log('response');
-			// 	console.log(response);
-			// 	return of({'invalid': true});
-			// },
-			// (error) => {
-			// 	console.log("error");
-			// 	console.log(error);
-			// 	return of(null);
-
-			// })
-
-
-			// return this.http.get('api/chat/find/' + control.value).pipe(
-			// map((response: any) => {
-				// console.log(response.headers.keys());
-				// console.log(response.body);
-				// console.log('response');
-				// console.log(response);
-				// return of({'invalid': true});
-			// }))
-
-
-
-
-
-
-			// return this.http.get('api/chat/find/' + control.value, {observe: 'response'})
-			// .pipe(map(res => {
-			// 		if (res instanceof HttpResponse && res.ok) {
-			// 			console.log("loll");
-			// 			return of(null);
-			// 		} else {
-			// 			console.log("loll");
-			// 			return of({'invalid': true});
-			// 		}
-			// 	}
-			// ));
-			if (control.value === "lol") {
-				return of({'invalid': true});
-			} else {
-				return of(null);
-			}
-
-			// console.log("here");
-			// return of(null);
-
-			// map(response => {
-				// console.log("hi");
-				// console.log(response.valueOf());
-				// return null;
-			// })
-		// }
-		// return of(null);
-	}
-
-		// return this.http.get('/api/chat/find/' + control.value)
-		// .pipe(
-		// 	debounceTime(500),
-		//   map( (data:any) =>  {
-		// 	  if (!data.isValid) return ({ 'InValid': true })
-		// 	//   else return null
-		//   })
-		// )
-	// }
-
-
-	
 	userForm = new FormGroup ({
 		username: new FormControl('', {
-			asyncValidators: [this.validateAsync.bind(this)]
 		})
-	}, {updateOn: "submit"})
-
+	})
 
     ngOnInit(): void {
-
 	}
 
 	public userNotFound: boolean = false;
     public submitUser() {
-		// console.log("form value: ", this.userForm.value.username);
         const newChat: createChatModel = {
             name: '',
             user: this.userForm.value.username
@@ -147,7 +51,6 @@ import { nullSafeIsEquivalent } from "@angular/compiler/src/output/output_ast";
                 console.log(error);
 				console.log(this.userForm.errors);
                 if (error.error.statusCode === 404) {
-					this.userForm.markAsTouched();
 					this.userNotFound = true;
 					console.log("user not found");
                 } else {
