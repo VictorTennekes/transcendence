6cf6f97d82e055159a6cd8fe666da5e1af3c63a0
saarikoski-jules <jsaariko@student.codam.nl>
2021-10-07 11:50:00 +0200

    fix: also logging otherwise invalid chat

--- descendence/src/app/chat/create-chat/create-chat.component.ts
+++ descendence/src/app/chat/create-chat/create-chat.component.ts
@@ -105,12 +105,10 @@ export class CreateChatComponent implements OnInit {
 				this.router.navigate(['', {outlets: {chat: ['get-chat', response.id]}}], {state: response});
 			},
 			(error) => {
-				console.log("erroring");
-				// this.submitted = false;
 				this.errorMessage = error.error.message;
 			})
 		} else {
-			console.log('invalid');
+			this.errorMessage = "form invalid"
 		}
 	}
 
