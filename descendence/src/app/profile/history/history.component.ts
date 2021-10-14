805971094f7b4e83b2e9236a9f48c9c69e70fec2
Tishj <t_b@live.nl>
2021-10-15 01:06:47 +0200

    fix: reduced scale of profile page, it was too big

--- descendence/src/app/profile/history/history.component.ts
+++ descendence/src/app/profile/history/history.component.ts
@@ -48,16 +48,22 @@ export class HistoryComponent implements OnInit {
 		ngOnInit(): void {
 			this.intra_name = this.route.snapshot.parent?.data.user.intra_name;
 			this.games = this.route.snapshot.data.history;
-			// const elem1: GameHistory = {
-			// 	winner: "Tishj",
-			// 	opponent: "Max Theorum",
-			// 	duration: 234,
-			// 	date: new Date(),
-			// 	score: {
-			// 		["Max Theorum"]: 3,
-			// 		["Tishj"]: 10
-			// 	},
-			// };
+			const elem1: GameHistory = {
+				winner: {
+					intra_name: "tbruinem",
+					display_name: "Tishj"
+				},
+				opponent: {
+					intra_name: "vihetu",
+					display_name: "Max Theorum",
+				},
+				duration: 234,
+				date: new Date(),
+				score: {
+					["vihetu"]: 3,
+					["tbruinem"]: 10
+				},
+			};
 			// const elem2: GameHistory = {
 			// 	winner: "Max Theorum",
 			// 	opponent: "Tishj",
@@ -70,14 +76,14 @@ export class HistoryComponent implements OnInit {
 			// };
 			// this.games.push(elem1);
 			console.log(`RESOLVED HISTORY: ${JSON.stringify(this.route.snapshot.data.history)}`);
-			// this.games.push(elem2);
+			// this.games.push(elem1);
+			// this.games.push(elem1);
+			// this.games.push(elem1);
 			// this.games.push(elem1);
 			// this.games.push(elem2);
-			// this.games.push(elem1);
 			// this.games.push(elem2);
-			// this.games.push(elem1);
 			// this.games.push(elem2);
-			// this.games.push(elem1);
+			// this.games.push(elem2);
 			// this.games.push(elem2);
 			// this.games.push(elem1);
 			// this.games.push(elem2);
