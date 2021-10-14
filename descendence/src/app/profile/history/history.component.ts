a4b3be80298041d40dc23a271aa5509e2a5cc061
Tishj <t_b@live.nl>
2021-10-14 14:26:58 +0200

    feat: profile page html/css finished

--- descendence/src/app/profile/history/history.component.ts
+++ descendence/src/app/profile/history/history.component.ts
@@ -1,15 +1,86 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/user.service';
+
+interface GameHistory {
+	winner: string;
+	opponent: string;
+	duration: number,
+	date: Date,
+	score: {[key: string] : number};
+};
+
+var toHHMMSS = (secs: number) => {
+	var sec_num = secs;
+	var hours   = Math.floor(sec_num / 3600)
+	var minutes = Math.floor(sec_num / 60) % 60
+	var seconds = sec_num % 60
+	
+	return [hours,minutes,seconds]
+	.map(v => v < 10 ? "0" + v : v)
+	.filter((v,i) => v !== "00" || i > 0)
+	.join(":")
+}
 
 @Component({
-  selector: 'app-history',
-  templateUrl: './history.component.html',
-  styleUrls: ['./history.component.scss']
+	selector: 'app-history',
+	templateUrl: './history.component.html',
+	styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+	
+	formatDateAndTime(timestamp: Date, seconds: number) {
+		const date = timestamp.toLocaleDateString('en-US');
+		return date + " - " + toHHMMSS(seconds);
+	}
+	
+	games: GameHistory[] = [];
+	display_name: string;
+	constructor(
+		private readonly userService: UserService
+		) { }
+		
+		isWinner(winner: string) {
+			return this.display_name === winner;
+		}
+		
+		ngOnInit(): void {
+			this.userService.getCurrentUser().subscribe((user: any) => {
+				this.display_name = user.display_name;
+			});
+			const elem1: GameHistory = {
+				winner: "Tishj",
+				opponent: "Max Theorum",
+				duration: 234,
+				date: new Date(),
+				score: {
+					["Max Theorum"]: 3,
+					["Tishj"]: 10
+				},
+			};
+			const elem2: GameHistory = {
+				winner: "Max Theorum",
+				opponent: "Tishj",
+				duration: 142,
+				date: new Date(),
+				score: {
+					["Max Theorum"]: 10,
+					["Tishj"]: 5
+				},
+			};
+			this.games.push(elem1);
+			this.games.push(elem2);
+			this.games.push(elem1);
+			this.games.push(elem2);
+			this.games.push(elem1);
+			this.games.push(elem2);
+			this.games.push(elem1);
+			this.games.push(elem2);
+			this.games.push(elem1);
+			this.games.push(elem2);
+			this.games.push(elem1);
+			this.games.push(elem2);
+			this.games.push(elem1);
+			this.games.push(elem2);
+		}
+	}
+	
