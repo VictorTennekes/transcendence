9eb909ea48836b35770efb9aab4aa8eb668c0828
Victor Tennekes <victor@tennekes.nl>
2021-10-19 15:12:46 +0200

    Fix: resolved #17

--- nestjs/src/match/match.service.ts
+++ nestjs/src/match/match.service.ts
@@ -110,6 +110,8 @@ export class MatchService {
 			if (this.excludedFromSearch(key))
 				continue ;
 			if (this.matches[key].settingCompare(settings)) {
+				if (this.matches[key].creator.login == user.login)
+					continue ;
 				this.matches[key].setOpponent(user);
 				return (key);
 			}
